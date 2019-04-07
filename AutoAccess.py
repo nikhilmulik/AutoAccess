d712c969b683a4fe618aaefffe7ca0bbf8100409
nikhilmulik2002 <nikhilmulik2002@gmail.com>
2019-04-06 21:59:01 -0400

    lint fix

--- AutoAccess.py
+++ AutoAccess.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def check():
     print "AutoAccess Server Running..."
-    return jsonify({'result': True})
+    return jsonify({'Server status': 'Running...'})
+
 
 @app.route('/light/on', methods=['GET'])
 def lighOn():
     print "Turning Light ON !"
     motorController.light_on()
     return jsonify({'result': True})
+
 
 @app.route('/light/on/interval/<interval>', methods=['GET'])
 def lighOnInterval(interval):
@@ -25,6 +27,7 @@ def lighOnInterval(interval):
     except:
         print "ERROR: Cannot read interval value"
         return jsonify({'result': False})
+
 
 @app.route('/light/off', methods=['GET'])
 def lighOff():
